bd85f57e926edfc799316a3447fe65726a563362
Frank van de Pol <fvdpol@gmail.com>
2026-07-03 14:33:53 -0400

    Formatting

--- ploytec_proto.h
+++ ploytec_proto.h
@@ -14,33 +14,38 @@
 
 /* Ploytec Protocol Constants */
 #define PLOYTEC_PKT_SIZE 512
-#define PLOYTEC_MIDI_IDLE_BYTE 0xFD
 
 /* Playback & MIDI Out (EP 0x05) */
-#define PLOYTEC_EP_NUM_PCM_OUT 0x05
-#define PLOYTEC_PLAYBACK_FRAMES 10	// number of samples per Frame
+#define PLOYTEC_EP_NUM_PCM_OUT	0x05
+#define PLOYTEC_PLAYBACK_FRAMES	10	// number of samples per Frame
 #define PLOYTEC_PLAYBACK_FRAME_SIZE 48
-#define PLOYTEC_MIDI_OUT_OFFSET 480
+#define PLOYTEC_MIDI_OUT_OFFSET 480	// location of MIDI data in the playback packet
+#define PLOYTEC_MIDI_IDLE_BYTE	0xFD	// send when no MIDI data is available
 #define PLOYTEC_SYNC_BYTE_OFFSET 481
 #define PLOYTEC_SYNC_BYTE_VALUE 0xFF
 
 /* Capture (EP 0x86) */
-#define PLOYTEC_EP_NUM_PCM_IN 0x06		// 0x86 & 0x0F
-#define PLOYTEC_CAPTURE_FRAMES 8	// number of samples per frame
+#define PLOYTEC_EP_NUM_PCM_IN	0x06		// 0x86 & 0x0F
+#define PLOYTEC_CAPTURE_FRAMES	8	// number of samples per frame
 #define PLOYTEC_CAPTURE_FRAME_SIZE 64
 
 /* MIDI In (EP 0x83) */
-#define PLOYTEC_EP_NUM_MIDI_IN 0x03		// 0x83 & 0x0F
+#define PLOYTEC_EP_NUM_MIDI_IN	0x03		// 0x83 & 0x0F
 
 /* Protocol Commands */
-#define PLOYTEC_SET_RATE     0x01
-#define PLOYTEC_SET_RATE_VAL 0x22
-#define PLOYTEC_REQ_STATUS   0x49
-#define PLOYTEC_REQ_FIRMWARE 0x56
-#define PLOYTEC_REQ_GET_RATE 0x81
+#define PLOYTEC_SET_RATE        0x01	// bRequest to set sample rate
+#define PLOYTEC_SET_RATE_TYPE   0x22	// bmRequestType to set sample rate
+#define PLOYTEC_REQ_STATUS      0x49	// bRequest to get device status
+#define PLOYTEC_REQ_FIRMWARE    0x56	// bRequest to get firmware version
+#define PLOYTEC_REQ_GET_RATE    0x81	// bRequest to get current sample rate
 
 /* Status Bits */
-#define PLOYTEC_STATUS_READY 0x20
+#define PLOYTEC_STATUS_BIT0     0x01
+#define PLOYTEC_STATUS_BIT1     0x02
+#define PLOYTEC_STATUS_BIT2     0x04
+#define PLOYTEC_STATUS_BIT3     0x08
+#define PLOYTEC_STATUS_BIT4     0x10
+#define PLOYTEC_STATUS_READY    0x20
 
 /* Bit-shuffling codec for S24_3LE (3 bytes per sample) */
 void ploytec_encode_s24_3le(u8 *dest, const u8 *src);
